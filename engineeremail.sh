920bbc37a373bd4d14e7e707d1e6978880aa957b
tomeschmusic <tomesch@gmail.com>
2021-02-18 03:40:01 +0000

    engineerbashemail

---
+++ engineeremail.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+################TOM ESCH###############
+
+# Prospect Mail (Outlook Client)
+sudo apt update
+sudo apt install snapd
+sudo snap install prospect-mail
+
+# Source:
+#https://websiteforstudents.com/install-prospect-mail-unofficial-outlook-365-client-on-ubuntu-18-04/
+
+# Thunderbird Client
+sudo apt-get install thunderbird
+
+# Source:
+# https://vitux.com/how-to-install-and-setup-thunderbird-email-client-in-ubuntu/
+
+###############TOM ESCH##################
